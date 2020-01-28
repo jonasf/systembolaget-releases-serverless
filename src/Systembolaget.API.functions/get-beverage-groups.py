8bc8b854ea626ffc859d236ebd9de6bf53490c05
Jonas Fredriksson <jonas.fredriksson@r2m.se>
2020-01-28 16:40:40 +0100

    Get beverage groups

--- src/Systembolaget.API.functions/get-beverage-groups.py
+++ src/Systembolaget.API.functions/get-beverage-groups.py
@@ -1,15 +1,23 @@
-""" import os
+import os
+import json
 import boto3
-"""
+from botocore.exceptions import ClientError
 
 
 def get_groups(event, context):
 
-    """ aws_region = os.environ['AWS_REGION']
+    aws_region = os.environ['AWS_REGION']
     dynamodb = boto3.resource('dynamodb', region_name=aws_region)
     table = dynamodb.Table('SystembolagetReleases')
-    """
     groups = []
+    try:
+        response = table.scan()
+    except ClientError as e:
+        print(e.response['Error']['Message'])
+    else:
+        for item in response['Items']:
+            if item['Group'] not in groups:
+                groups.append(item['Group'])
     return {
-        'data': groups
+        'data': json.dumps(groups)
     }
