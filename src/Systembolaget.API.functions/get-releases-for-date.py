7c41be5bc3a441e4ceddd4fd06ad6c4e9787fbf5
Jonas Fredriksson <jonas.fredriksson@r2m.se>
2020-01-28 11:46:01 +0100

    Get beverages from releases table

--- src/Systembolaget.API.functions/get-releases-for-date.py
+++ src/Systembolaget.API.functions/get-releases-for-date.py
@@ -1,7 +1,41 @@
+import os
+import boto3
+import json
+import decimal
+from botocore.exceptions import ClientError
+
+
+class DecimalEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, decimal.Decimal):
+            if o % 1 > 0:
+                return float(o)
+            else:
+                return int(o)
+        return super(DecimalEncoder, self).default(o)
+
+
 def get_releases(event, context):
-    message = 'Release date: {}, beverage group: {}'.format(event['date'],
-                                                            event['group'])
+    release_date = event['date']
+    beverage_group = event['group']
+    aws_region = os.environ['AWS_REGION']
+    dynamodb = boto3.resource('dynamodb', region_name=aws_region)
+    table = dynamodb.Table('SystembolagetReleases')
+    beverages = []
+    try:
+        response = table.get_item(
+            Key={
+                    'ReleaseDate': release_date,
+                    'Group': beverage_group
+                }
+        )
+    except ClientError as e:
+        print(e.response['Error']['Message'])
+    else:
+        if 'Item' in response:
+            beverages = json.dumps(response['Item']['Beverages'],
+                                   indent=4, cls=DecimalEncoder)
 
     return {
-        'message': message
+        'data': beverages
     }
