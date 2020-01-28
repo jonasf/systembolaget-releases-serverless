6a3d0bc6d8baaa8e0788802cfbe12e72f35ac1b9
Jonas Fredriksson <jonas.fredriksson@r2m.se>
2020-01-28 15:44:25 +0100

    Fighting with the linter

--- src/Systembolaget.API.functions/get-beverage-groups.py
+++ src/Systembolaget.API.functions/get-beverage-groups.py
@@ -1,12 +1,14 @@
-import os
+""" import os
 import boto3
+"""
 
 
 def get_groups(event, context):
 
-    aws_region = os.environ['AWS_REGION']
+    """ aws_region = os.environ['AWS_REGION']
     dynamodb = boto3.resource('dynamodb', region_name=aws_region)
     table = dynamodb.Table('SystembolagetReleases')
+    """
     groups = []
     return {
         'data': groups
