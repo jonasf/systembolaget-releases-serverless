c00e0407c57ae7242e6b25182af4f51b1096f943
Jonas Fredriksson <jonas.fredriksson@r2m.se>
2020-01-28 13:06:21 +0100

    Set the date format explicit

--- src/Systembolaget.Releases.Indexer/DataSource/ReleasesDataSource.cs
+++ src/Systembolaget.Releases.Indexer/DataSource/ReleasesDataSource.cs
@@ -41,7 +41,7 @@ namespace Systembolaget.Releases.Indexer.DataSource
                 }
 
                 var document = new Document();
-                document["ReleaseDate"] = release.ReleaseDate.ToShortDateString();
+                document["ReleaseDate"] = release.ReleaseDate.ToString("yyyy-MM-dd");
                 document["Group"] = release.Group;
                 document["NumberOfBeverages"] = release.NumberOfBeverages;
                 document["Beverages"] = beverages;
