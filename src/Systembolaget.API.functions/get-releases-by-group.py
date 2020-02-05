5686f74f6d32213686cae70b21e654833b1c29f5
Jonas Fredriksson <l.jonas.fredriksson@gmail.com>
2020-02-05 16:47:46 +0100

    Read releases for group

--- src/Systembolaget.API.functions/get-releases-by-group.py
+++ src/Systembolaget.API.functions/get-releases-by-group.py
@@ -1,6 +1,20 @@
 import os
 import boto3
+import json
+from json import JSONEncoder
+from boto3.dynamodb.conditions import Key
 from botocore.exceptions import ClientError
+
+
+class MyEncoder(JSONEncoder):
+    def default(self, o):
+        return o.__dict__
+
+
+class Release:
+    def __init__(self, date, count):
+        self.date = date
+        self.count = count
 
 
 def get_releases(event, context):
@@ -8,19 +22,19 @@ def get_releases(event, context):
     aws_region = os.environ['AWS_REGION']
     dynamodb = boto3.resource('dynamodb', region_name=aws_region)
     table = dynamodb.Table('SystembolagetReleases')
-    beverages = []
+    releases = []
     try:
-        response = table.get_item(
-            Key={
-                    'Group': beverage_group
-                }
+        response = table.query(
+            IndexName='GroupIndex',
+            KeyConditionExpression=Key('Group').eq(beverage_group)
         )
     except ClientError as e:
         print(e.response['Error']['Message'])
     else:
-        if 'Item' in response:
-            beverages = response['Item']['Beverages']
+        for item in response['Items']:
+                releases.append(Release(item['ReleaseDate'],
+                                str(item['NumberOfBeverages'])))
 
     return {
-        'data': beverages
+        'data': json.dumps(releases, cls=MyEncoder)
     }
