e2ac72df45c992b896d9749de34de7e4d22cbb85
Jonas Fredriksson <l.jonas.fredriksson@gmail.com>
2020-02-15 17:29:42 +0100

    Set up Bootstrap

---
+++ src/systembolaget.releases.web/src/About.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export class About extends Component {
+    static displayName = About.name;
+
+    render() {
+        return (
+            <div>About this site.</div>
+        );
+    }
+}
--- src/systembolaget.releases.web/src/App.js
+++ src/systembolaget.releases.web/src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import { Layout } from './Layout';
+import { Home } from './Home';
+import { About } from './About';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default class App extends Component {
+  static displayName = App.name;
 
-export default App;
+  render () {
+    return (
+      <Router>
+        <Layout>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/about' component={About} />
+        </Layout>
+      </Router>
+    );
+  }
+}
