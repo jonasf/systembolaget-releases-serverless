2aa15b7f917f670ddb5aef2859c0fe83b2ca0e78
Jonas Fredriksson <l.jonas.fredriksson@gmail.com>
2020-02-22 14:23:48 +0100

    Fix font size

--- src/systembolaget.releases.web/src/About.js
+++ src/systembolaget.releases.web/src/About.js
@@ -8,7 +8,7 @@ export class About extends Component {
             <div>
                 <h1>Om</h1>
                 <p>Den här sidan ger en enkel översikt över alla nyheter på Systembolaget.</p>
-                <p>Systembolagets <a href="https://www.systembolaget.se/api/" target="_blank">öppna API</a> används som datakälla.</p>
+                <p>Systembolagets <a href="https://www.systembolaget.se/api/" target="_blank" rel="noopener noreferrer">öppna API</a> används som datakälla.</p>
             </div>
         );
     }
